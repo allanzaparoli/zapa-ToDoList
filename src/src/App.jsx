313648689d77fb044dc6f1df30f2d229a7bc8fa2
allanzaparoli <allanzaparoli@gmail.com>
2023-07-19 16:50:47 -0300

    iniciando projeto

---
+++ src/src/App.jsx
@@ -0,0 +1,13 @@
+import { Form } from './components/form.jsx';
+
+import './global.css';
+
+export function App() {
+  return (
+    <div>
+      <main>
+        <Form />
+      </main>
+    </div>
+  );
+}
