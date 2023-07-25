30616ca1162e787d69390adc32f5d9193fb7172b
allanzaparoli <allanzaparoli@gmail.com>
2023-07-25 15:25:13 -0300

    implementando código

--- src/src/components/CommentsText.jsx
+++ src/src/components/CommentsText.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import trash from '../assets/trash.svg';
+import check from '../assets/check.svg';
+import styles from './CommentsText.module.css';
 
-export function CommentsText(content) {
+export function CommentsText({
+  content,
+}) {
   return (
-    <section>
-      <p>{content}</p>
+    <section className={styles.CommentsText}>
+      <div className={styles.checkText}>
+        <img src={check} alt="check" />
+        <p>{content}</p>
+      </div>
+      <div className={styles.trash}>
+        <img src={trash} alt="lixo" />
+      </div>
     </section>
   );
 }
